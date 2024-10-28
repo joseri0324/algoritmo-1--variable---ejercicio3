b3fc8c916d89ae0e12b19e05bd62a7b7adfc96e1
jose <josesaa0324@gmail.com>
2024-10-27 23:22:48 -0500

    crear formula con mensaje alert

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const numsecreto = Math.floor(Math.random() * 100) + 1; 
+
+
+function adivinarNumero() {
+    
+    let suposicion = parseInt(prompt("Adivina el número entre 1 y 100:"));
+
+    while (suposicion !== numSecreto) {
+        if (suposicion < numSecreto) {
+            alert("¡Muy bajo! Intenta con un número más alto.");
+        } else if (suposicion > numSecreto) {
+            alert("¡Muy alto! Intenta con un número más bajo.");
+        }
+
+        
+        suposicion = parseInt(prompt("Intenta de nuevo:"));
+    }
+
+   
+    alert("¡Felicidades! Adivinaste el número secreto: " + numSecreto);
+}
+
+adivinarNumero();
